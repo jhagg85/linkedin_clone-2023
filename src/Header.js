66db0deee463d3c40ec6fda3ce3414a93dd4337d
JosephGarganera <josephgarganera@JosephGarganeras-MacBook-Pro.local>
2024-01-02 23:16:43 +0800

    starting sidebar

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,41 @@
 import React from "react";
 import "./Header.css";
-import { SearchOutlined } from "@mui/icons-material";
+import {
+  BusinessCenter,
+  Chat,
+  Home,
+  Notifications,
+  SearchOutlined,
+  SupervisorAccount,
+} from "@mui/icons-material";
+import HeaderOption from "./HeaderOption";
 
 function Header() {
   return (
     <div className="header">
-      <h1>This is the header</h1>
-
       <div className="header__left">
-        <img src="" alt="" />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png"
+          alt=""
+        />
 
         <div className="header__search">
           <SearchOutlined />
-
           <input type="text" />
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={Home} title="Home" />
+        <HeaderOption Icon={SupervisorAccount} title="My Network" />
+        <HeaderOption Icon={BusinessCenter} title="Jobs" />
+        <HeaderOption Icon={Chat} title="Messaging" />
+        <HeaderOption Icon={Notifications} title="Notifications" />
+        <HeaderOption
+          avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiF9NSs6EJut5KFIouheuFHJz7WTBY2OgjUfRNsf3_9pQejcgSHOxksN4iYPWldl2iUfc&usqp=CAU"
+          title="Joseph"
+        />
+      </div>
     </div>
   );
 }
